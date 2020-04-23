c05f9b91403d108bb75ce6401f790f03c87ea18b
ahuana20 <ahuana20@gmail.com>
2020-04-23 00:24:26 -0500

    agrego var entorno

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 app = Flask(__name__)
 
+nombre = os.environ['nombre']
 
 @app.route('/')
 def index():
-    return f"hola mundo, soy"
+    return f"hola mundo, soy {nombre}...."
 
 if __name__ == '__main__':
   app.run(port=80, debug=False)
